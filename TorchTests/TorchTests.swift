83ffa02301c519bdf6789e8d6198bf5156a37730
Deszip <raf.rafiki@gmail.com>
2024-07-04 18:47:18 +0300

    Initial Commit

---
+++ TorchTests/TorchTests.swift
@@ -0,0 +1,17 @@
+//
+//  TorchTests.swift
+//  TorchTests
+//
+//  Created by Deszip on 04.07.2024.
+//
+
+import Testing
+@testable import Torch
+
+struct TorchTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
